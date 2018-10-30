f7d1596a031de0a31d93ecaa41aff8abb1dc21f8
Noah Wilkie <noahwilkie@gmail.com>
2018-10-30 09:44:38 -0700

    New files added

--- CH5/Ex/page161.py
+++ CH5/Ex/page161.py
@@ -1,18 +1,12 @@
-line = "101;Johnny 'wave-boy' Jones;USA;8.32;Fish;21"
-id,name,country,avgscore,board,age = line.split(";")
+line = "101;johnny 'wave-boy' Jones;USA;8.32;Fish;21"
 
 s = {}
-s["id"] = id
-s["name"] = name
-s["country"] = country
-s["avg"] = avgscore
-s["board"] = board
-s["age"] = age
+s['id'],s['name'],s['country'],s['avgscore'],s['board'],s['age'] = line.split(";")
 
 print("ID:         ",s["id"])
 print("Name:       ",s["name"])
 print("Country:    ",s["country"])
-print("Average:    ",s["avg"])
+print("Average:    ",s["avgscore"])
 print("Board Type: ",s["board"])
 print("Age:        ",s["age"])
 print(s)
