7ae1fdba0664258f9169d300cd14c2cf559ade5e
Noah Wilkie <noahwilkie@gmail.com>
2018-11-20 16:33:09 -0800

    Added a Audio only download button

--- Misc/pythonguitest.py
+++ Misc/pythonguitest.py
@@ -23,6 +23,25 @@ def vidDownload():
             #print(err) # debugging purposes only
             messagebox.showerror("Error!","Please specify a valid Youtube URL")
 
+def audioDL():
+    cmd = "youtube-dl -s -f bestaudio -o "
+    directory = f_path.get()
+
+    if directory == "Select a Download Location " or directory == '':
+        messagebox.showerror("Error!", "Please choose a valid Directory!")
+    else:
+
+        Label(app, text="Downloading!").grid()
+
+        try:
+            subprocess.check_call(cmd + directory + "\%(title)s.%(ext)s " + link.get())
+            Label(app, text="Complete!").grid(column="1")
+
+        except subprocess.CalledProcessError:
+            # print(err) # debugging purposes only
+            messagebox.showerror("Error!", "Please specify a valid Youtube URL")
+
+
 def browse():
     global f_path
     directory = askdirectory()
@@ -31,14 +50,14 @@ def browse():
 #Start of Program
 app = Tk()
 app.title("Youtube-dl GUI")
-app.geometry("450x125")
+app.geometry("550x125")
 
 #Download Location
 f_path = StringVar()
 f_path.set("Select a Download Location ")
 Label(app,text="Download Location:").grid(column="1")
 Entry(app, textvariable=f_path, state="readonly",width="50").grid(column="1",padx='20',pady='2')
-Button(app, text="Browse", command=browse).grid(column="2", row="1")
+Button(app, text="Browse", command=browse,width=15).grid(column="2", row="1")
 
 #Url
 Label(app, text="Video Url:").grid(column="1",row="2")
@@ -46,7 +65,9 @@ link = Entry(app, text="Url..",width="50")
 link.grid(column="1",row="3")
 
 #Download Button
-Button(app, text="Download", command=vidDownload).grid(column="2",row="3")
+Button(app, text="Download Video", command=vidDownload,width=15).grid(column="2",row="3")
+Button(app, text="Download Audio", command=audioDL,width=15).grid(column="3",row="3")
+
 
 app.mainloop()
 
